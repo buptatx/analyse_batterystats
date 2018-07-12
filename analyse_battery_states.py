e99007301cca6535b16a151821126a4b8b0a2111
zhangpeng <zhp@chinabr.net>
2018-07-12 18:52:27 +0800

    add xlsx modify code

--- analyse_battery_states.py
+++ analyse_battery_states.py
@@ -28,12 +28,6 @@ def get_estimated_drain(filename):
     if end_idx == 0:
         end_idx = start_idx + 30
 
-    print("cur_idx: %d" % cur_idx)
-    print("start_idx: %d" % start_idx)
-    print("end_idx :%d" % end_idx)
-    print("target_proc :%s" % target_proc)
-
-
     power_content = content[start_idx:end_idx+1]
     power_estimated = {}
     for line in power_content:
@@ -64,33 +58,81 @@ def walk_data_dir(data_path):
             if len(res) == 0:
                 continue
             res["scene"] = name
+            res["desc"] = get_scene_desc(name)
             result.append(res)
 
     result.sort(key=lambda x:(-x["target"], -x["total_drain"]))
     store_excel(result)
 
 
+def get_scene_desc(name):
+	if "no_internet_wpw_per_5_min" in name and "bgm" not in name:
+		return "断网每5分钟播放一次唤醒词"
+	elif "wifi_wpw_per_5_min" in name and "bgm" not in name:
+		return "wifi每5分钟播放一次唤醒词"
+	elif "4G_wpw_per_5_min" in name and "bgm" not in name:
+		return "4G每5分钟播放一次唤醒词"
+	elif "no_internet_wpw_per_5_min_with_bgm" in name:
+		return "断网播放背景音乐每5分钟播放一次唤醒词"
+	elif "wifi_wpw_per_5_min_with_bgm" in name:
+		return "wifi播放背景音乐每5分钟播放一次唤醒词"
+	elif "4G_wpw_per_5_min_with_bgm" in name:
+		return "4G播放背景音乐每5分钟播放一次唤醒词"
+	elif "4G_on_xj_view" in name:
+		return "4G在小简页面静等待1小时"
+	elif "wifi_on_xj_view" in name:
+		return "wifi在小简页面静等待1小时"
+	elif "no_internet_on_xj_view" in name:
+		return "断网在小简页面静等待1小时"
+	elif "4G_on_launcher_with_xj_app" in name:
+		return "4G在原生桌面静等待1小时"
+	elif "4G_without_xj_app" in name:
+		return "4G未安装小简app"
+	else:
+		return "未知场景"
+
+
 def store_excel(res):
     wbk = xlwt.Workbook()
     sht = wbk.add_sheet('estimated')
+    data_size = xlwt.easyxf("font:height 280")
+
+    sht.write(0, 0, "场景", data_size)
+    sht.write(0, 1, "描述", data_size)
+    sht.write(0, 2, "总消耗(mAh)", data_size)
+    sht.write(0, 3, "屏幕消耗(mAh)", data_size)
+    sht.write(0, 4, "小简消耗(mAh)", data_size)
+    sht.write(0, 5, "移动网络(mAh)", data_size)
+    sht.write(0, 6, "Wifi(mAh)", data_size)
+
+    char_width = 269
+    sht.col(0).width = char_width * 60
+    sht.col(1).width = char_width * 50
+    sht.col(2).width = char_width * 20
+    sht.col(3).width = char_width * 20
+    sht.col(4).width = char_width * 20
+    sht.col(5).width = char_width * 20
+    sht.col(6).width = char_width * 20
+
     cur_line = 1
 
     for item in res:
         if len(item) == 1:
             continue
 
-        sht.write(cur_line, 0, item["scene"])
-        sht.write(cur_line, 1, item["total_drain"])
-        sht.write(cur_line, 2, item["screen"])
-        sht.write(cur_line, 3, item["target"])
+        sht.write(cur_line, 0, item["scene"], data_size)
+        sht.write(cur_line, 1, item["desc"], data_size)
+        sht.write(cur_line, 2, item["total_drain"], data_size)
+        sht.write(cur_line, 3, item["screen"], data_size)
+        sht.write(cur_line, 4, item["target"], data_size)
         if "cell" in item:
-            sht.write(cur_line, 4, item["cell"])
+            sht.write(cur_line, 5, item["cell"], data_size)
         else:
-            sht.write(cur_line, 4, 0)
+            sht.write(cur_line, 5, 0, data_size)
         if "wifi" in  item:
-            sht.write(cur_line, 5, item["wifi"])
+            sht.write(cur_line, 6, item["wifi"], data_size)
         else:
-            sht.write(cur_line, 5, 0)
+            sht.write(cur_line, 6, 0, data_size)
 
         cur_line += 1
 
@@ -100,4 +142,3 @@ def store_excel(res):
 
 if __name__ == "__main__":
     walk_data_dir("./data")
-    #get_estimated_drain("./data/wifi_on_xj_view.txt")
